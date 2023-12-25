a33540b4073777bb5894e295c357de5076184083
Руслан Мусаев <ru.mu2005@mail.ru>
2023-12-25 15:39:27 +0500

    Task5

--- Tyuiu.MusaevRS.Sprint1.Task5.V3.Lib/DataService.cs
+++ Tyuiu.MusaevRS.Sprint1.Task5.V3.Lib/DataService.cs
@@ -3,11 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using tyuiu.cources.programming.interfaces.Sprint1;
 
 
 namespace Tyuiu.MusaevRS.Sprint1.Task5.V3.Lib
 {
-    public class DataService
+    public class DataService : ISprint1Task5V3
     {
+        public int Calculate(int k)
+        {
+            return (k / 100) % 10;
+
+
+
+        }
     }
 }
